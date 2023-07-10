f0a595c173b269acaee25c014d3acaac6198b08b
Tanushrimeshram19 <tanushrimeshram47@gmail.com>
2023-07-10 13:18:19 +0530

    file upload

---
+++ MVC/WeeklyTypeex/WeeklyTypeex/Controllers/UsersController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WeeklyTypeex.Controllers
+{
+    public class UsersController : Controller
+    {
+        // GET: Users
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult form1(int txtId, string txtName, string chkAddon)
+        {
+            ViewBag.Id = txtId;
+            ViewBag.Name = txtName;
+            if (chkAddon != null)
+                ViewBag.Addon = "Selected";
+            else
+                ViewBag.Addon = "Not Selected";
+
+            return View("Index");
+        }
+    }
+}
